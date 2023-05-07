f53c3aa65caf6e588feadc4ddd0d24c93066173c
mjay <michael@merged.group>
2023-05-06 23:10:36 -0400

    other stuff

--- src/modules/ezmanage-subscriber/ezmanage-subscriber.service.ts
+++ src/modules/ezmanage-subscriber/ezmanage-subscriber.service.ts
@@ -10,7 +10,14 @@ import { IEventNotificationPayload } from './interfaces';
 export class EzmanageSubscriberService {
   async handleWebhook(payload: IEventNotificationPayload) {
     try {
-      const { parent_type, entity_type, key } = payload;
+      const {
+        parent_type,
+        parent_id,
+        entity_type,
+        entity_id,
+        key,
+        occurred_at,
+      } = payload;
       /**
        * Note:
        * If the "Menu Updated" subscription isn't required for this system,
@@ -38,20 +45,58 @@ export class EzmanageSubscriberService {
         );
       }
 
+      /**
+       * If here, parent_type is 'Caterer', entity_type is 'Order', and key is either 'accepted' or 'cancelled'
+       * Thus, parent_id should be catererId, entityId should be orderId
+       */
+
       if (key === EventNotificationPayloadKey.CANCELLED)
-        return this.handleOrderCancelled();
+        return this.handleOrderCancelled(entity_id);
 
       /**
        * Should be order accepted
        */
-      return this.handleOrderAccepted();
+      return this.handleOrderAccepted({
+        catererId: parent_id,
+        orderId: entity_id,
+        occurredAt: occurred_at,
+      });
     } catch (err) {
       console.error('err', err);
       throw err;
     }
   }
 
-  private async handleOrderAccepted() {}
+  private async handleOrderCancelled(orderId: string) {
+    /**
+     * Delete from system
+     */
+  }
 
-  private async handleOrderCancelled() {}
+  /**
+   * This handles order accepted events, which include
+   * 1) Initial acceptance
+   * 2) Order update acceptance
+   */
+  private async handleOrderAccepted({
+    catererId,
+    orderId,
+    occurredAt,
+  }: {
+    catererId: string;
+    orderId: string;
+    occurredAt: string;
+  }) {
+    /**
+     * Is orderId in system?
+     *
+     * If not:
+     * Make GraphQL query to confirm that order is not completed
+     * If order is not completed, add it to DB
+     *
+     * If so:
+     * Make GraphQL query to check order details
+     * Is order completed?  Then ready to
+     */
+  }
 }
