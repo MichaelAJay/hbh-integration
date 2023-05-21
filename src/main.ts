bc541c8653e2577ff1ccdd536a5298f7eb1d0854
mjay <michael@merged.group>
2023-05-21 13:27:20 -0400

    updated CORS policy

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,39 @@
-import { ValidationPipe } from '@nestjs/common';
+import { InternalServerErrorException, ValidationPipe } from '@nestjs/common';
+import {
+  CorsOptions,
+  CorsOptionsDelegate,
+} from '@nestjs/common/interfaces/external/cors-options.interface';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 async function bootstrap() {
+  const { ENV, FRONTEND_BASE_URL } = process.env;
+  if (!(ENV && FRONTEND_BASE_URL))
+    throw new InternalServerErrorException(
+      'Server missing required environment variables',
+    );
+  const corsOrigin = [FRONTEND_BASE_URL];
+  if (ENV === 'local' || ENV === 'dev')
+    corsOrigin.push('http://localhost:8080', 'https://localhost:8080');
+
+  const corsOptions: CorsOptions | CorsOptionsDelegate<any> = {
+    origin: corsOrigin,
+    credentials: true,
+    allowedHeaders: [
+      'Access-Control-Allow-Origin',
+      'Origin',
+      'X-Requested-With',
+      'Accept',
+      'Content-Type',
+      'Authorization',
+    ],
+    exposedHeaders: 'Authorization',
+    methods: ['GET', 'PUT', 'OPTIONS', 'POST', 'DELETE', 'PATCH'],
+  };
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.enableCors({ origin: 'myfrontend', credentials: true });
+  app.enableCors(corsOptions);
 
   app.useGlobalPipes(
     // new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
