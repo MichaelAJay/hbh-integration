38d72da595dce0e396fdbbb06412ad2f363ee1b2
mjay <michael@merged.group>
2023-07-20 21:47:39 -0400

    finished user controller unit tests

--- src/api/user/user.controller.unit.spec.ts
+++ src/api/user/user.controller.unit.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { Response } from 'express';
 import { AuthService } from 'src/internal-modules/auth/auth.service';
+import { IRefreshAuthenticationRequest } from '../interfaces';
 import { LoginBodyDto } from './dtos/body';
 import { UserController } from './user.controller';
 import { UserAPIService } from './user.service';
@@ -54,12 +55,12 @@ describe('UserController', () => {
         send: jest.fn(),
       } as unknown as Response;
 
-      const mockResolvedValues = {
+      const mockResolvedValue = {
         at: 'MOCK AT',
         rt: 'MOCK RT',
       };
 
-      jest.spyOn(userService, 'login').mockResolvedValue(mockResolvedValues);
+      jest.spyOn(userService, 'login').mockResolvedValue(mockResolvedValue);
       await controller.login(mockBody, mockResponse);
       expect(userService.login).toHaveBeenCalledWith(mockBody);
     });
@@ -148,7 +149,166 @@ describe('UserController', () => {
       expect(result).toEqual(mockResolvedValue);
     });
   });
-  describe('POST /refresh-auth', () => {});
-  describe('PATCH /claim-account', () => {});
+  describe('POST /refresh-auth', () => {
+    it('calls userService.refreshAuth with the correct arguments', async () => {
+      const mockReq = {
+        userId: 'MOCK USER ID',
+        rt: 'MOCK REFRESH TOKEN',
+      } as IRefreshAuthenticationRequest;
+
+      const mockResponse = {
+        cookie: jest.fn(),
+        send: jest.fn(),
+      } as unknown as Response;
+
+      const mockResolvedValue = {
+        at: 'MOCK AT',
+        rt: 'MOCK RT',
+      };
+      jest
+        .spyOn(userService, 'refreshAuth')
+        .mockResolvedValue(mockResolvedValue);
+      await controller.refreshAuth(mockReq, mockResponse);
+      expect(userService.refreshAuth).toHaveBeenCalledWith({
+        userId: mockReq.userId,
+        rt: mockReq.rt,
+      });
+    });
+    it('calls Response method cookie (2) on successful userService.refreshAuth Promise resolution', async () => {
+      const mockReq = {
+        userId: 'MOCK USER ID',
+        rt: 'MOCK REFRESH TOKEN',
+      } as IRefreshAuthenticationRequest;
+
+      const mockResponse = {
+        cookie: jest.fn(),
+        send: jest.fn(),
+      } as unknown as Response;
+
+      const mockResolvedValue = {
+        at: 'MOCK AT',
+        rt: 'MOCK RT',
+      };
+      jest
+        .spyOn(userService, 'refreshAuth')
+        .mockResolvedValue(mockResolvedValue);
+
+      jest.spyOn(mockResponse, 'cookie');
+      await controller.refreshAuth(mockReq, mockResponse);
+
+      expect(mockResponse.cookie).toHaveBeenCalledTimes(2);
+      expect(mockResponse.cookie).toHaveBeenCalledWith(
+        'accessToken',
+        mockResolvedValue.at,
+        {
+          httpOnly: true,
+          sameSite: 'lax',
+          path: '/',
+        },
+      );
+      expect(mockResponse.cookie).toHaveBeenCalledWith(
+        'refreshToken',
+        mockResolvedValue.rt,
+        {
+          httpOnly: true,
+          sameSite: 'lax',
+          path: '/',
+        },
+      );
+    });
+    it('propagates any error thrown by userService.refreshAuth', async () => {
+      const mockReq = {
+        userId: 'MOCK USER ID',
+        rt: 'MOCK REFRESH TOKEN',
+      } as IRefreshAuthenticationRequest;
+
+      const mockResponse = {
+        cookie: jest.fn(),
+        send: jest.fn(),
+      } as unknown as Response;
+
+      const mockError = new Error('ERROR UNDER TEST');
+      jest.spyOn(userService, 'refreshAuth').mockRejectedValue(mockError);
+      await expect(
+        controller.refreshAuth(mockReq, mockResponse),
+      ).rejects.toThrow(mockError);
+    });
+    it('returns res.send with an object containing the results from userService.refreshAuth', async () => {
+      const mockReq = {
+        userId: 'MOCK USER ID',
+        rt: 'MOCK REFRESH TOKEN',
+      } as IRefreshAuthenticationRequest;
+
+      const mockResponse = {
+        cookie: jest.fn(),
+        send: jest.fn(),
+      } as unknown as Response;
+
+      const mockResolvedUserServiceRefreshAuth = {
+        at: 'MOCK AT',
+        rt: 'MOCK RT',
+      };
+      const mockResolvedValue = JSON.stringify(
+        mockResolvedUserServiceRefreshAuth,
+      );
+      jest
+        .spyOn(userService, 'refreshAuth')
+        .mockResolvedValue(mockResolvedUserServiceRefreshAuth);
+
+      jest.spyOn(mockResponse, 'send');
+      const result = await controller.refreshAuth(mockReq, mockResponse);
+      expect(mockResponse.send).toHaveBeenCalledWith({
+        at: mockResolvedUserServiceRefreshAuth.at,
+        rt: mockResolvedUserServiceRefreshAuth.rt,
+      });
+    });
+  });
+  describe('PATCH /claim-account', () => {
+    it('calls userService.claimAccount with the correct arguments', async () => {
+      const mockBody = {
+        token: 'MOCK TOKEN',
+        newPassword: 'MOCK NEW PASSWORD',
+      };
+
+      const mockResolvedValue = {
+        msg: 'Account claimed.  You may log in with your new password',
+      };
+      jest
+        .spyOn(userService, 'claimAccount')
+        .mockResolvedValue(mockResolvedValue);
+
+      await controller.claimAccount(mockBody);
+      expect(userService.claimAccount).toHaveBeenCalledWith(mockBody);
+    });
+    it('passes through the Promise from userService.claimAccount if it succeeds', async () => {
+      const mockBody = {
+        token: 'MOCK TOKEN',
+        newPassword: 'MOCK NEW PASSWORD',
+      };
+
+      const mockResolvedValue = {
+        msg: 'Account claimed.  You may log in with your new password',
+      };
+      jest
+        .spyOn(userService, 'claimAccount')
+        .mockResolvedValue(mockResolvedValue);
+
+      const result = await controller.claimAccount(mockBody);
+      expect(result).toEqual(mockResolvedValue);
+    });
+    it('propagates any error thrown by userService.claimAccount', async () => {
+      const mockBody = {
+        token: 'MOCK TOKEN',
+        newPassword: 'MOCK NEW PASSWORD',
+      };
+
+      const mockError = new Error('ERROR UNDER TEST');
+      jest.spyOn(userService, 'claimAccount').mockRejectedValue(mockError);
+
+      await expect(controller.claimAccount(mockBody)).rejects.toThrow(
+        mockError,
+      );
+    });
+  });
   afterEach(() => jest.restoreAllMocks());
 });
