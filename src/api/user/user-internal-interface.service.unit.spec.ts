9f036f4fb78dd4819a84bb8bc0f11310835115ac
mjay <michael@merged.group>
2023-07-23 15:18:01 -0400

    passing tests

--- src/api/user/user-internal-interface.service.unit.spec.ts
+++ src/api/user/user-internal-interface.service.unit.spec.ts
@@ -1,7 +1,16 @@
+import {
+  BadRequestException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import {
+  IAccountModelWithId,
+  IUserModelWithId,
+} from 'src/external-modules/database/models';
 import { AuthService } from 'src/internal-modules/auth/auth.service';
 import { AccountDbHandlerService } from 'src/internal-modules/external-interface-handlers/database/account-db-handler/account-db-handler.service';
 import { UserDbHandlerService } from 'src/internal-modules/external-interface-handlers/database/user-db-handler/user-db-handler.service';
+import { ILogin } from './interfaces';
 import { UserInternalInterfaceService } from './user-internal-interface.service';
 
 describe('UserInternalInterfaceService', () => {
@@ -16,15 +25,26 @@ describe('UserInternalInterfaceService', () => {
         UserInternalInterfaceService,
         {
           provide: UserDbHandlerService,
-          useValue: {},
+          useValue: {
+            getOneByEmail: jest.fn(),
+            getOne: jest.fn(),
+            updateOne: jest.fn(),
+          },
         },
         {
           provide: AccountDbHandlerService,
-          useValue: {},
+          useValue: {
+            getAccount: jest.fn(),
+          },
         },
         {
           provide: AuthService,
-          useValue: {},
+          useValue: {
+            signAuthToken: jest.fn(),
+            signRefreshToken: jest.fn(),
+            hashValue: jest.fn(),
+            hashedValueGate: jest.fn(),
+          },
         },
       ],
     }).compile();
@@ -47,8 +67,193 @@ describe('UserInternalInterfaceService', () => {
     test('AuthService to be injected into service.authService', () =>
       expect(authService).toBeDefined());
   });
-  describe('login', () => {});
+  describe('login', () => {
+    it('calls userDbhandler.getOneByEmail with the correct arguments', async () => {
+      const mockArguments: ILogin = {
+        username: 'MOCK USERNAME (EMAIL)',
+        password: 'MOCK PASSWORD',
+      };
+      const mockUser: IUserModelWithId = {
+        id: 'MOCK USER ID',
+        accountId: 'MOCK ACCOUNT ID',
+        firstName: 'MOCK FIRST NAME',
+        lastName: 'MOCK LAST NAME',
+        email: mockArguments.username,
+        hashedPassword: 'MOCK HASHED PASSWORD',
+        salt: 'MOCK SALT',
+      };
+      const mockAccount: IAccountModelWithId = {
+        id: mockUser.accountId,
+        ref: 'ADMIN',
+        name: 'MOCK ACCOUNT',
+        contactEmail: 'MOCK CONTACT EMAIL',
+        isActive: true,
+      };
+
+      jest.spyOn(userDbHandler, 'getOneByEmail').mockResolvedValue(mockUser);
+      jest.spyOn(accountDbHandler, 'getAccount').mockResolvedValue(mockAccount);
+      jest.spyOn(authService, 'hashedValueGate').mockResolvedValue(undefined);
+      await service.login(mockArguments);
+      expect(userDbHandler.getOneByEmail).toHaveBeenCalledWith(
+        mockArguments.username,
+      );
+    });
+    it('calls accountDbHandler.getAccount with the correct arguments', async () => {
+      const mockArguments: ILogin = {
+        username: 'MOCK USERNAME (EMAIL)',
+        password: 'MOCK PASSWORD',
+      };
+      const mockUser: IUserModelWithId = {
+        id: 'MOCK USER ID',
+        accountId: 'MOCK ACCOUNT ID',
+        firstName: 'MOCK FIRST NAME',
+        lastName: 'MOCK LAST NAME',
+        email: mockArguments.username,
+        hashedPassword: 'MOCK HASHED PASSWORD',
+        salt: 'MOCK SALT',
+      };
+      const mockAccount: IAccountModelWithId = {
+        id: mockUser.accountId,
+        ref: 'ADMIN',
+        name: 'MOCK ACCOUNT',
+        contactEmail: 'MOCK CONTACT EMAIL',
+        isActive: true,
+      };
+
+      jest.spyOn(userDbHandler, 'getOneByEmail').mockResolvedValue(mockUser);
+      jest.spyOn(accountDbHandler, 'getAccount').mockResolvedValue(mockAccount);
+      jest.spyOn(authService, 'hashedValueGate').mockResolvedValue(undefined);
+      await service.login(mockArguments);
+      expect(accountDbHandler.getAccount).toHaveBeenCalledWith(
+        mockUser.accountId,
+      );
+    });
+    it('throws UnprocessableEntityException if accountDbhandler.getAccount returns null', async () => {
+      const mockArguments: ILogin = {
+        username: 'MOCK USERNAME (EMAIL)',
+        password: 'MOCK PASSWORD',
+      };
+      const mockUser: IUserModelWithId = {
+        id: 'MOCK USER ID',
+        accountId: 'MOCK ACCOUNT ID',
+        firstName: 'MOCK FIRST NAME',
+        lastName: 'MOCK LAST NAME',
+        email: mockArguments.username,
+        hashedPassword: 'MOCK HASHED PASSWORD',
+        salt: 'MOCK SALT',
+      };
+      const mockAccount = null;
+
+      jest.spyOn(userDbHandler, 'getOneByEmail').mockResolvedValue(mockUser);
+      jest.spyOn(accountDbHandler, 'getAccount').mockResolvedValue(mockAccount);
+      jest.spyOn(authService, 'hashedValueGate').mockResolvedValue(undefined);
+      await expect(service.login(mockArguments)).rejects.toThrow(
+        new UnprocessableEntityException('Could not find account'),
+      );
+    });
+    it('throws BadRequestException if userDbHandler.getOneByEmail returns a record without hashedPassword', async () => {
+      const mockArguments: ILogin = {
+        username: 'MOCK USERNAME (EMAIL)',
+        password: 'MOCK PASSWORD',
+      };
+      const mockUser: Omit<IUserModelWithId, 'hashedPassword'> = {
+        id: 'MOCK USER ID',
+        accountId: 'MOCK ACCOUNT ID',
+        firstName: 'MOCK FIRST NAME',
+        lastName: 'MOCK LAST NAME',
+        email: mockArguments.username,
+        salt: 'MOCK SALT',
+      };
+      const mockAccount: IAccountModelWithId = {
+        id: mockUser.accountId,
+        ref: 'ADMIN',
+        name: 'MOCK ACCOUNT',
+        contactEmail: 'MOCK CONTACT EMAIL',
+        isActive: true,
+      };
+
+      jest
+        .spyOn(userDbHandler, 'getOneByEmail')
+        .mockResolvedValue(mockUser as IUserModelWithId);
+      jest.spyOn(accountDbHandler, 'getAccount').mockResolvedValue(mockAccount);
+      jest.spyOn(authService, 'hashedValueGate').mockResolvedValue(undefined);
+      await expect(service.login(mockArguments)).rejects.toThrow(
+        new BadRequestException('No match'),
+      );
+    });
+    it('calls authService.hashedValueGate and then returns tokens object if hashedValueGate does not error', async () => {
+      const mockArguments: ILogin = {
+        username: 'MOCK USERNAME (EMAIL)',
+        password: 'MOCK PASSWORD',
+      };
+      const mockUser: IUserModelWithId = {
+        id: 'MOCK USER ID',
+        accountId: 'MOCK ACCOUNT ID',
+        firstName: 'MOCK FIRST NAME',
+        lastName: 'MOCK LAST NAME',
+        email: mockArguments.username,
+        hashedPassword: 'MOCK HASHED PASSWORD',
+        salt: 'MOCK SALT',
+      };
+      const mockAccount: IAccountModelWithId = {
+        id: mockUser.accountId,
+        ref: 'ADMIN',
+        name: 'MOCK ACCOUNT',
+        contactEmail: 'MOCK CONTACT EMAIL',
+        isActive: true,
+      };
+
+      jest.spyOn(userDbHandler, 'getOneByEmail').mockResolvedValue(mockUser);
+      jest.spyOn(accountDbHandler, 'getAccount').mockResolvedValue(mockAccount);
+      jest.spyOn(authService, 'hashedValueGate').mockResolvedValue(undefined);
+      await service.login(mockArguments);
+      expect(authService.hashedValueGate).toHaveBeenCalledWith({
+        hashedValue: mockUser.hashedPassword,
+        valueToHash: mockArguments.password,
+        salt: mockUser.salt,
+      });
+    });
+
+    /**
+     * TODO
+     */ it("propagates ForbiddenException from authService.hashedValueGate if incoming password and stored password hashes don't match", async () => {
+      const mockArguments: ILogin = {
+        username: 'MOCK USERNAME (EMAIL)',
+        password: 'MOCK PASSWORD',
+      };
+      const mockUser: IUserModelWithId = {
+        id: 'MOCK USER ID',
+        accountId: 'MOCK ACCOUNT ID',
+        firstName: 'MOCK FIRST NAME',
+        lastName: 'MOCK LAST NAME',
+        email: mockArguments.username,
+        hashedPassword: 'MOCK HASHED PASSWORD',
+        salt: 'MOCK SALT',
+      };
+      const mockAccount: IAccountModelWithId = {
+        id: mockUser.accountId,
+        ref: 'ADMIN',
+        name: 'MOCK ACCOUNT',
+        contactEmail: 'MOCK CONTACT EMAIL',
+        isActive: true,
+      };
+
+      jest.spyOn(userDbHandler, 'getOneByEmail').mockResolvedValue(mockUser);
+      jest.spyOn(accountDbHandler, 'getAccount').mockResolvedValue(mockAccount);
+      jest.spyOn(authService, 'hashedValueGate').mockResolvedValue(undefined);
+      await service.login(mockArguments);
+      expect(accountDbHandler.getAccount).toHaveBeenCalledWith(
+        mockUser.accountId,
+      );
+    });
+  });
+  /**
+   * TODO
+   */
   describe('refreshAuth', () => {});
-  describe('resetPassword', () => {});
+
+  /**
+   * TODO
+   */ describe('resetPassword', () => {});
   afterEach(() => jest.restoreAllMocks());
 });
--- src/api/user/user.service.unit.spec.ts
+++ src/api/user/user.service.unit.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ILogin, IResetPassword } from './interfaces';
 import { UserInternalInterfaceService } from './user-internal-interface.service';
-import { UserApiModule } from './user.module';
 import { UserAPIService } from './user.service';
 
 describe('UserService', () => {
@@ -13,7 +13,11 @@ describe('UserService', () => {
         UserAPIService,
         {
           provide: UserInternalInterfaceService,
-          useValue: {},
+          useValue: {
+            login: jest.fn(),
+            refreshAuth: jest.fn(),
+            resetPassword: jest.fn(),
+          },
         },
       ],
     }).compile();
@@ -29,8 +33,142 @@ describe('UserService', () => {
     test('UserInternalInterfaceService is injected into service.userInternalInterface', () =>
       expect(userInternalInterface).toBeDefined());
   });
-  describe('login', () => {});
-  describe('refreshAuth', () => {});
-  describe('claimAccount', () => {});
+  describe('login', () => {
+    it('calls userInternalInterface.login with the correct arguments', async () => {
+      const mockArguments: ILogin = {
+        username: 'MOCK USER NAME',
+        password: 'MOCK PASSWORD',
+      };
+      const mockResolvedValue = {
+        at: 'MOCK ACCESS TOKEN',
+        rt: 'MOCK REFRESH TOKEN',
+      };
+
+      jest.spyOn(userInternalInterface, 'login');
+      await service.login(mockArguments);
+      expect(userInternalInterface.login).toHaveBeenCalledWith(mockArguments);
+    });
+    it('passes through the result from userInternalInterface.login on success', async () => {
+      const mockArguments: ILogin = {
+        username: 'MOCK USER NAME',
+        password: 'MOCK PASSWORD',
+      };
+      const mockError = new Error('ERROR UNDER TEST');
+
+      jest.spyOn(userInternalInterface, 'login').mockRejectedValue(mockError);
+      await expect(service.login(mockArguments)).rejects.toThrow(mockError);
+    });
+    it('propagates any error thrown by userInternalInterface.login', async () => {
+      const mockArguments: ILogin = {
+        username: 'MOCK USER NAME',
+        password: 'MOCK PASSWORD',
+      };
+      const mockResolvedValue = {
+        at: 'MOCK ACCESS TOKEN',
+        rt: 'MOCK REFRESH TOKEN',
+      };
+
+      jest
+        .spyOn(userInternalInterface, 'login')
+        .mockResolvedValue(mockResolvedValue);
+      const result = await service.login(mockArguments);
+      expect(result).toEqual(mockResolvedValue);
+    });
+  });
+  describe('refreshAuth', () => {
+    it('calls userInternalInterface.refreshAuth with the correct arguments', async () => {
+      const mockArguments = {
+        userId: 'MOCK USER ID',
+        rt: 'MOCK REFRESH TOKEN',
+      };
+      const mockResolvedValue = {
+        at: 'MOCK ACCESS TOKEN',
+        rt: 'MOCK REFRESH TOKEN',
+      };
+      jest
+        .spyOn(userInternalInterface, 'refreshAuth')
+        .mockResolvedValue(mockResolvedValue);
+
+      await service.refreshAuth(mockArguments);
+      expect(userInternalInterface.refreshAuth).toHaveBeenCalledWith(
+        mockArguments,
+      );
+    });
+    it('passes through the result from userInternalInterface.refreshAuth on success', async () => {
+      const mockArguments = {
+        userId: 'MOCK USER ID',
+        rt: 'MOCK REFRESH TOKEN',
+      };
+      const mockResolvedValue = {
+        at: 'MOCK ACCESS TOKEN',
+        rt: 'MOCK REFRESH TOKEN',
+      };
+      jest
+        .spyOn(userInternalInterface, 'refreshAuth')
+        .mockResolvedValue(mockResolvedValue);
+
+      const result = await service.refreshAuth(mockArguments);
+      expect(result).toEqual(mockResolvedValue);
+    });
+    it('propagates any error thrown by userInternalInterface.refreshAuth', async () => {
+      const mockArguments = {
+        userId: 'MOCK USER ID',
+        rt: 'MOCK REFRESH TOKEN',
+      };
+      const mockError = new Error('ERROR UNDER TEST');
+      jest
+        .spyOn(userInternalInterface, 'refreshAuth')
+        .mockRejectedValue(mockError);
+
+      await expect(service.refreshAuth(mockArguments)).rejects.toThrow(
+        mockError,
+      );
+    });
+  });
+  describe('claimAccount', () => {
+    it('calls userInternalInterface.resetPassword with the correct arguments', async () => {
+      const mockArguments: IResetPassword = {
+        token: 'MOCK RESET PASSWORD TOKEN',
+        newPassword: 'MOCK NEW PASSWORD',
+      };
+      const mockResolvedValue = undefined;
+      jest
+        .spyOn(userInternalInterface, 'resetPassword')
+        .mockResolvedValue(mockResolvedValue);
+      await service.claimAccount(mockArguments);
+      expect(userInternalInterface.resetPassword).toHaveBeenCalledWith(
+        mockArguments,
+      );
+    });
+    it('sends success message object if userInternalInterface.resetPassword successfully resolved', async () => {
+      const mockSuccessMessageObject = {
+        msg: 'Account claimed.  You may log in with your new password',
+      };
+
+      const mockArguments: IResetPassword = {
+        token: 'MOCK RESET PASSWORD TOKEN',
+        newPassword: 'MOCK NEW PASSWORD',
+      };
+      const mockResolvedValue = undefined;
+      jest
+        .spyOn(userInternalInterface, 'resetPassword')
+        .mockResolvedValue(mockResolvedValue);
+      const result = await service.claimAccount(mockArguments);
+      expect(result).toEqual(mockSuccessMessageObject);
+    });
+    it('propagates any error thrown by userInternalInterface.resetPassword', async () => {
+      const mockArguments: IResetPassword = {
+        token: 'MOCK RESET PASSWORD TOKEN',
+        newPassword: 'MOCK NEW PASSWORD',
+      };
+      const mockError = new Error('ERROR UNDER TEST');
+      jest
+        .spyOn(userInternalInterface, 'resetPassword')
+        .mockRejectedValue(mockError);
+      await expect(service.claimAccount(mockArguments)).rejects.toThrow(
+        mockError,
+      );
+    });
+  });
   afterEach(() => jest.restoreAllMocks());
 });
